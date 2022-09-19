b3e8b4ead9c0d3881d48c40b70b54ab6d4f88c21
Ali Sherifi <ali.sherifi@live.com>
2022-09-20 00:51:18 +0200

    Close navigation bar after links clicked

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+/* close navigation bar after click on links */
+
+navbarLinks.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
