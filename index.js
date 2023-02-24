8a9d74211c855ee184e28b5d08f676334eee50d3
Ali Sherifi <ali.sherifi@live.com>
2023-02-24 16:40:58 -0600

    Close navbar when clicking outside the navbar

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ toggleBtn.addEventListener('click', () => {
 navbarLinks.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+/* close navigation bar when clicking outside navbar */
+document.addEventListener('click', (event) => {
+    // Check if the clicked element is inside the navbar or the toggle button
+    let isClickInsideNavbar = navbarLinks.contains(event.target) || toggleBtn.contains(event.target);
+    
+    // If the click is not inside the navbar, remove the 'active' class from navbarLinks
+    if (!isClickInsideNavbar) {
+        navbarLinks.classList.remove('active');
+    }
+});
