10e0df353b39dd165c4e0db734f86990fee683a4
Ali Sherifi <ali.sherifi@live.com>
2023-03-02 17:08:58 -0600

    Add animation to the offer-section

--- index.js
+++ index.js
@@ -30,3 +30,45 @@ let bannerText = document.querySelector('.banner-text');
 setTimeout(function() {
     bannerText.classList.add('show');
   }, 250);
+
+
+
+
+/* Animation offer */
+// Select the offer section and offer items
+const offerSection = document.querySelector('#offer-section');
+const offerItems = document.querySelectorAll('.offer-item');
+
+// Function to fade in the offer items
+function fadeInElements() {
+  // Loop through each offer item
+  offerItems.forEach((item, index) => {
+    // Add a delay to stagger the fade-in effect
+    setTimeout(() => {
+      // Add the fade-in-offer class to the offer item
+      item.classList.add('fade-in-offer');
+    }, 250 * index); // The delay is proportional to the index of the item
+  });
+}
+
+// Function to handle the intersection of the offer section
+function handleIntersection(entries) {
+  entries.forEach(entry => {
+    // If the section is in view
+    if (entry.isIntersecting) {
+      // Call the function to fade in the offer items
+      fadeInElements();
+    }
+  });
+}
+
+// Create a new IntersectionObserver object to observe the offer section
+const observer = new IntersectionObserver(handleIntersection, {
+  root: null, // Use the viewport as the root
+  threshold: 0.5 // Trigger the intersection at 50% visibility
+});
+
+// Observe the offer section with the observer object
+observer.observe(offerSection);
+
+  
