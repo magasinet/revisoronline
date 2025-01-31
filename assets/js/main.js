c77511723f2c643a55fb585e6164a0ad3462fd77
Ali Sherifi <ali.sherifi@live.com>
2025-01-31 12:08:01 +0100

    Add cookie banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,3 +171,42 @@
       faqItem.parentNode.classList.toggle('faq-active');
     });
   });
+
+
+
+// Set a cookie
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+// Get a cookie
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+// Check cookie on load
+document.addEventListener('DOMContentLoaded', (event) => {
+  if(getCookie('cookieAccepted') === 'true') {
+      document.querySelector('.cookie-banner').style.display = 'none';
+  }
+});
+
+// Event listener for the accept button
+document.querySelector('.close-btn').addEventListener('click', function() {
+  setCookie('cookieAccepted', 'true', 7); // Set cookie for 7 days
+  document.querySelector('.cookie-banner').style.display = 'none';
+});
+
